ad47c05de491ee3dcee7fa055b63f9dfdfa368c5
ujjwal0903 <ujjwalsinghal0903@gmail.com>
2025-02-05 15:49:59 +0530

    Navbar Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
+import userAuthForm from "./pages/userAuthForm.page";
 
 const App = () => {
     return (
-        <Navbar />
+        <Routes>
+            <Route path="/" element={<Navbar/>}> 
+                <Route path="signin" element={<userAuthForm type ="signin"/>} />
+                <Route path="signup" element={<userAuthForm type ="signup"/>} />
+            </Route>
+        </Routes>
     )
 }
 
