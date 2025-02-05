eb147994075c423d086f1b52854fdc7be2ba9264
ujjwal0903 <ujjwalsinghal0903@gmail.com>
2025-02-05 12:07:38 +0530

    Required Packages installed

---
+++ frontend/src/components/navbar.component.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+import Logo from "../imgs/logo.png"
+
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <Link to='/' className="flex-none w-10">
+                <img src={Logo} alt="Logo" className="w-full"/>
+            </Link>
+
+            <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw]">
+                <input type="text" placeholder="Search" className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-[6%] rounded-full placeholder:text-dark-grey" />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
